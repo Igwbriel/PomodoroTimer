2a2577146501b31455c1c04f8236ef660f83276e
Gabriel Dias <GabrielDias7820@gmail.com>
2023-09-25 15:34:03 -0300

    inputs Done

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -49,6 +49,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calender-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
