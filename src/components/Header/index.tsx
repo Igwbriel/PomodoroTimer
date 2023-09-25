ebe433ac593f837d33f92ad3d7b442c8b3860caa
Gabriel Dias <GabrielDias7820@gmail.com>
2023-09-24 22:42:38 -0300

    Finalização do Header, com NavLink e hovers

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
 import { HeaderContainer } from './styles'
 import LogoIgnite from '../../assets/Logo-ignite.svg'
 import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <HeaderContainer>
       <img src={LogoIgnite} />
       <nav>
-        <a href="">
+        <NavLink to="/" title="Timer">
           <Timer size={24} />
-        </a>
+        </NavLink>
 
-        <a href="">
+        <NavLink to="/History" title="Histórico">
           <Scroll size={24} />
-        </a>
+        </NavLink>
       </nav>
     </HeaderContainer>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -8,18 +8,27 @@ export const HeaderContainer = styled.header`
   nav {
     display: flex;
     gap: 0.5rem;
-  }
 
-  a {
-    width: 3rem;
-    height: 3rem;
+    a {
+      width: 3rem;
+      height: 3rem;
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-    color: ${(props) => props.theme['gray-100']};
-    border-top: 3px solid transparent;
-    border-bottom: 3px solid transparent;
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      &:hover {
+        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
   }
 `
