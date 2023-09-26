a2e4bc41c2ee24c4a0f7f1253322f701b278a7f4
Gabriel Dias <GabrielDias7820@gmail.com>
2023-09-26 11:27:05 -0300

    Funcionalidade do Formulário iniciada utilizando a biblioteca hook form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
 import {
   CountdownContainer,
   FormContainer,
@@ -10,15 +11,25 @@ import {
 } from './styles'
 
 export function Home() {
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCicle(data: unknown) {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCicle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -37,6 +48,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -50,7 +62,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StarCountdownButton disabled type="submit">
+        <StarCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           começar
         </StarCountdownButton>
